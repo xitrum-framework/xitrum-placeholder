333c401c7334ce321048c2e9260db9f0a06e0404
Takeharu Oshida <takeharu.oshida@mobilus.co.jp>
2013-09-03 21:43:24 +0900

    add siteIndex

--- src/main/scala/placeholder/Boot.scala
+++ src/main/scala/placeholder/Boot.scala
@@ -1,11 +1,12 @@
 package placeholder
 
 import org.jboss.netty.handler.codec.http.HttpHeaders
-
-import placeholder.model.Canvas
 import xitrum.ActionActor
 import xitrum.Server
 import xitrum.annotation.GET
+import xitrum.validator._
+import xitrum.exception.InvalidInput
+import placeholder.model._
 
 object Boot {
   def main(args: Array[String]) {
@@ -14,21 +15,48 @@ object Boot {
 }
 
 @GET("/")
-class AdminIndex extends ActionActor {
-  def execute() {
-    respondInlineView(<p>Welcome</p>)
-  }
+class SiteIndex extends ActionActor{
+  def execute() =
+    respondView()
 }
 
-@GET("/:width/:height")
-class HelloActor extends ActionActor {
+@GET("/:width")
+class WidthActor extends ActionActor {
   def execute() {
-    val width = param("width")
-    val height = param("height")
-    val bytes = Canvas.render(width.toInt, height.toInt)
+    val width = param[Int]("width")
+    val bytes = Canvas.render(width, width)
     response.setHeader(HttpHeaders.Names.CONTENT_TYPE, "image/png")
     response.setHeader(HttpHeaders.Names.CONTENT_LENGTH, bytes.length)
     respondBinary(bytes);
   }
 }
 
+@GET("/:width/:height")
+class HeightActor extends ActionActor {
+  def execute() {
+    val width = param[Int]("width")
+    val height = param[Int]("height")
+    val bytes = Canvas.render(width, height)
+    response.setHeader(HttpHeaders.Names.CONTENT_TYPE, "image/png")
+    response.setHeader(HttpHeaders.Names.CONTENT_LENGTH, bytes.length)
+    respondBinary(bytes);
+  }
+}
+
+@GET("/:width/:height/:option")
+class OptionActor extends ActionActor {
+  def execute() {
+    val width = param[Int]("width")
+    val height = param[Int]("height")
+    val option = param("option")
+
+    // TODO : handle option
+    // option : color=white&text=hogehoge&textcolor=red&key=value
+
+
+    val bytes = Canvas.render(width, height)
+    response.setHeader(HttpHeaders.Names.CONTENT_TYPE, "image/png")
+    response.setHeader(HttpHeaders.Names.CONTENT_LENGTH, bytes.length)
+    respondBinary(bytes);
+  }
+}
