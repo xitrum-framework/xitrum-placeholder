964b7b36d7d63fc394773c9e0a69e1eee0225957
Takeharu.Oshida <georgeosddev@gmail.com>
2013-09-06 01:03:43 +0900

    style change

--- src/main/scala/placeholder/model/Canvas.scala
+++ src/main/scala/placeholder/model/Canvas.scala
@@ -73,7 +73,7 @@ class CanvasActor extends Actor with Logger {
     var fontSize = if (rectangle.getHeight < 100) 10 else 20
     g.setFont(new Font("Verdana", Font.BOLD, fontSize))
     var s = rectangle.getText
-    s = if ("placeholder" == s) rectangle.getWidth.toString + " X " + rectangle.getHeight.toString else s
+    s = if ("placeholder" == s) rectangle.getWidth.toString + "X" + rectangle.getHeight.toString else s
     val fm = g.getFontMetrics
     val x = rectangle.getWidth/2 - fm.stringWidth(s)/2
     val y = rectangle.getHeight/2 + fm.getHeight()/2
@@ -107,17 +107,18 @@ class CanvasActor extends Actor with Logger {
     g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
     g2.setColor(circle.getColor)
     g2.fillOval(0, 0, circle.getRadius*2, circle.getRadius*2)
-    g.drawImage(img, 0, 0, null);
 
     var fontSize = if (circle.getRadius*2 < 30) 10 else 20
-    g.setFont(new Font("Verdana", Font.BOLD, fontSize))
+    g2.setFont(new Font("Verdana", Font.BOLD, fontSize))
     var s = circle.getText
     s = if ("placeholder" == s) circle.getRadius.toString else s
-    val fm = g.getFontMetrics
+    val fm = g2.getFontMetrics
     val x = circle.getRadius - fm.stringWidth(s)/2
     val y = circle.getRadius + fm.getHeight()/2
-    g.setColor(circle.getTextColor)
-    g.drawString(s, x, y);
+    g2.setColor(circle.getTextColor)
+    g2.drawString(s, x, y);
+
+    g.drawImage(img, 0, 0, null);
 
     g.dispose()
     val baos = new ByteArrayOutputStream()
